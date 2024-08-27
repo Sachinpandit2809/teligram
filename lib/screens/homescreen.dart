0313d2f112b993965bb7f2a9358b062b7079e62e
Sachinpandit2809 <sachinpandit2485@gmail.com>
2024-08-28 00:26:31 +0530

    added home Screen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -11,11 +11,76 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-        title: Text("1 Story"),
-        actions: [Icon(Icons.search)],
-      ),
-    );
+        drawer: const Drawer(),
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade600,
+          title: const Text("Teligram"),
+          actions: const <Widget>[
+            Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            SizedBox(width: 20)
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // dp
+                              SizedBox(
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.blue.shade700,
+                                  child: const Text("T"),
+                                ),
+                              )
+                              // data
+                              ,
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                        "Sachin Pandit sadhkasdklfkasdv;jasldj"),
+                                    Text("#video comments"),
+                                  ],
+                                ),
+                              ),
+                              const Spacer(),
+                              //time & notice number
+                              Column(
+                                children: [
+                                  Text(
+                                      "${DateTime.now().hour}:${DateTime.now().minute}"),
+                                  const CircleAvatar(
+                                    radius: 12,
+                                    child: Text("1"),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                            indent: 70,
+                          )
+                        ],
+                      );
+                    }),
+              )
+            ],
+          ),
+        ));
   }
 }
